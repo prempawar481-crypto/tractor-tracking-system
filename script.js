0a8dfd9b9c8aff50edebc2129cd808ed04e2098e
prempawar481-crypto <prempawar481@gmail.com>
2026-06-30 13:35:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const customers = [
 
+{
+phone:"7899902735",
+name:"Basamma W/o Manappa",
+tractor:"Mahindra 575 DI",
+location:"Bangalore",
+status:"Running"
+},
+
+{
+phone:"7204258784",
+name:"Ravikumar S/o Hanamantraya Hosur",
+tractor:"John Deere 5050 Gear Pro",
+location:"Pune",
+status:"Running"
+},
+
+{
+phone:"9148305884",
+name:"KanthaReddy S/o MallikarjunReddy",
+tractor:"Mahindra 575 DI",
+location:"Bangalore",
+status:"Running"
+},
+
+{
+phone:"9901036121",
+name:"Jayashree",
+tractor:"Swaraj 744 XT",
+location:"Bangalore",
+status:"Running"
+},
+
+{
+phone:"8088883730",
+name:"Vishwanath S/o Ningappa",
+tractor:"Massey Ferguson 7250 50 HP",
+location:"Doddaballapur",
+status:"Running"
+},
