d2d6028d6b276e59b7cc0b4d8dc444e5fa36c9a0
prempawar481-crypto <prempawar481@gmail.com>
2026-06-30 06:41:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-function showMessage() {
-  alert("🚜 Tractor Tracking System is working successfully!");
+function searchTractor() {
+  const input = document.getElementById("search").value.toLowerCase();
+  const tractors = document.querySelectorAll(".tractor-card");
+
+  tractors.forEach(card => {
+    if (card.innerText.toLowerCase().includes(input)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+function updateLocation(button) {
+  const locations = [
+    "Yadgir",
+    "Kalaburagi",
+    "Raichur",
+    "Hubballi",
+    "Bengaluru",
+    "Shivamogga",
+    "Ballari"
+  ];
+
+  const status = ["Running", "Stopped"];
+
+  const card = button.parentElement;
+  const location = card.querySelector(".location");
+  const state = card.querySelector(".status");
+
+  location.innerHTML =
+    "<b>Location:</b> " +
+    locations[Math.floor(Math.random() * locations.length)];
+
+  state.innerHTML =
+    "<b>Status:</b> " +
+    status[Math.floor(Math.random() * status.length)];
 }
