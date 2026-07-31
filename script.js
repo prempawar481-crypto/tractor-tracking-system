f9326a1c974157e509f6330bad6c261a31745ecf
prempawar481-crypto <prempawar481@gmail.com>
2026-07-31 10:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const customers = [
+{
+  phone: "8495038473",
+  name: "Mahantappa S/o Guranna",
+  tractor: "Mahindra 575 DI",
+  location: "Bangalore",
+  status: "PDI Verification"
+}
+];
 
+function searchCustomer() {
+  let phone = document.getElementById("phoneSearch").value.trim();
+
+  let customer = customers.find(c => c.phone === phone);
+
+  if (!customer) {
+    alert("Customer Not Found");
+    return;
+  }
+
+  document.getElementById("customerDetails").style.display = "block";
+  document.getElementById("customerName").innerHTML = customer.name;
+  document.getElementById("customerPhone").innerHTML = customer.phone;
+  document.getElementById("tractorModel").innerHTML = customer.tractor;
+  document.getElementById("currentLocation").innerHTML = customer.location;
+  document.getElementById("currentStatus").innerHTML = customer.status;
+}
