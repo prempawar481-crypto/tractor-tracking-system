65c1785f4477ce87d87d4016b8f152de457f77d3
prempawar481-crypto <prempawar481@gmail.com>
2026-08-05 19:12:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,16 @@ function searchCustomer() {
   }
 
   document.getElementById("customerDetails").style.display = "block";
+  let steps = document.querySelectorAll(".step");
+steps.forEach(step => step.classList.remove("active"));
+
+if(customer.status === "Documents Verification"){
+    steps[0].classList.add("active");
+}
+else if(customer.status === "PDI Verification"){
+    steps[0].classList.add("active");
+    steps[1].classList.add("active");
+}
 
   document.getElementById("customerName").innerHTML = customer.name;
   document.getElementById("tractorModel").innerHTML = customer.tractor;
